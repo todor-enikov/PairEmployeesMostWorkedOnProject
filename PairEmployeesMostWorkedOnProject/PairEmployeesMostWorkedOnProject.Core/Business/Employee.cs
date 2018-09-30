76369774261d353796912e68629a9e9fb5299b78
Enikov <enigmaenikov@gmail.com>
2018-09-30 11:44:38 +0300

    Implemented initial class Employee.

---
+++ PairEmployeesMostWorkedOnProject/PairEmployeesMostWorkedOnProject.Core/Business/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PairEmployeesMostWorkedOnProject.Core.Business
+{
+    public class Employee
+    {
+        private int employeeID;
+
+        private DateTime dateFrom;
+
+        private DateTime dateTo;
+
+        public Employee(int employeeID, DateTime dateFrom, DateTime dateTo)
+        {
+            this.EmployeeID = employeeID;
+            this.DateFrom = dateFrom;
+            this.DateTo = dateTo;
+        }
+
+        public int EmployeeID
+        {
+            get { return this.employeeID; }
+            set { this.employeeID = value; }
+        }
+
+        public DateTime DateFrom
+        {
+            get { return this.dateFrom; }
+            set { this.dateFrom = value; }
+        }
+
+        public DateTime DateTo
+        {
+            get { return this.dateTo; }
+            set { this.dateTo = value; }
+        }
+    }
+}
